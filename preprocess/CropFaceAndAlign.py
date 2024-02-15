f50045262ac2f5ed384bf421558d207436fbcc19
AmitCohen313 <50398606+AmitCohen313@users.noreply.github.com>
2024-02-15 10:56:20 +0200

    Update CropFaceAndAlign.py

--- preprocess/CropFaceAndAlign.py
+++ preprocess/CropFaceAndAlign.py
@@ -1,36 +1,3 @@
-#!/usr/bin/env python
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2012, Philipp Wagner
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of the author(s) nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
 import sys, math
 from PIL import Image
 
@@ -86,5 +53,5 @@ def CropFace(image, eye_left=(0,0), eye_right=(0,0), offset_pct=(0.35,0.35), des
   return image
 
 if __name__ == "__main__":
-  image =  Image.open("/Users/amitcohen/Downloads/NIR-VIS-2.0/s1/NIR/00202/013.bmp")
+  image =  Image.open("<PATH TO IMAGE>")
   CropFace(image, eye_left=(160,352), eye_right=(282,342), dest_sz=(224,224)).save("test.jpg")
