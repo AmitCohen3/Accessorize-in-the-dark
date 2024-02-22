67aeb4f5eec48bea679a929c496fb215935aeb10
AmitCohen313 <50398606+AmitCohen313@users.noreply.github.com>
2024-02-22 20:45:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import utils
 from arg_parser import Parser
 from load_model import load_model
 from attacks import perform_attack
+from predict import predict
 from utils import init_plots_dir, prepare_data_paths, save_configuration
 
 
@@ -13,9 +14,12 @@ def main():
     protocols_path = args.protocols_folder_name
     gallery_file_path, probe_file_path = prepare_data_paths(args.dataset_path, protocols_path, args.gallery_index)
 
-    print("Started! Plots will be in ", utils.plots_dir)
-    acc_before_attack, acc_after_attack, target_att_succ_rate = perform_attack(args, model, gallery_file_path, probe_file_path)
-    print(f"Accuracy before attack: {acc_before_attack} || Accuracy after attack: {acc_after_attack} || Targeted attack success rate: {target_att_succ_rate}")
+    print("Started! Plots will be in", utils.plots_dir)
+    if args.predict:
+        predict(args, model, gallery_file_path, probe_file_path)
+    else:
+        acc_before_attack, acc_after_attack, target_att_succ_rate = perform_attack(args, model, gallery_file_path, probe_file_path)
+        print(f"Accuracy before attack: {acc_before_attack} || Accuracy after attack: {acc_after_attack} || Targeted attack success rate: {target_att_succ_rate}")
     print("Finished! Plots are in ", utils.plots_dir)
 
 if __name__ == '__main__':
